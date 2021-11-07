d47b891f1ca56988955dbcb2192406cf3c3c1106
Benedek Harsanyi <hben.0204@gmail.com>
2021-11-07 15:37:14 +0100

    fix bugs

--- gambler.py
+++ gambler.py
@@ -1,60 +1,54 @@
 import numpy as np
-import random
+import matplotlib.pyplot as plt
 
-def value_iteration(threshold, gamma, p):
+value_function = np.zeros(101)
+policy = np.zeros(101)
+states = np.arange(0,101)
+rewards = np.zeros(101)
+rewards[-1] = 1
+gamma = 1
+threshold = 0.0001
+
+def value_iteration(p, max_sweep):
     """value iteration algo for solving the gamblers problem"""
     # initialization
-    delta = 100
-    value_function = np.zeros(100)
-    states = np.arange(1,100)
-    rewards = np.zeros(100)
-    # termination states
-    rewards[0] = -1 
-    rewards[99] = 1
-
+    delta = 1
+    
     # iterative update of the value function using the bellman equations
     sweep = 0
-    while delta > threshold:
+    while delta > threshold and sweep<max_sweep:
+        delta = 0
         #one sweep over the state space
-        print(f'calculating sweep {sweep}')
+        #print(f'calculating sweep {sweep}')
         for state in states:
-            
             current_value = value_function[state]
-            value_function[state] = update_policy(value_function, state, gamma, p)
+            value_function[state] = update_policy(state, p)
             delta = max(delta, np.abs(current_value - value_function[state]))
-        sweep += 1
+        sweep = sweep + 1
 
     # compute a deterministic policy using the value function
-    optimal_policy = compute_optimal_policy(gamma, value_function)
+    #optimal_policy = compute_optimal_policy(value_function, gamma, p, rewards)
 
-    return optimal_policy
+    return policy, value_function
 
+def compute_expected_return_for_state_action(state, action, p):
+    """computes the expected return for a state action pair using the Bellman equations"""   
+    return p * (rewards[state + action] + gamma * value_function[state + action]) + (1 - p) * (0 + gamma * value_function[state - action])
 
-def update_policy(value_function, state, gamma, p):
+def update_policy(state, p):
     """evaluating the expected return for each action, and choose greedily"""
     expected_returns = []
     actions = np.arange(0, min(state, 100 - state) + 1)
     for action in actions:
-        expected_returns.append(compute_expected_return_for_state_action(state, action, gamma, p, value_function))
+        expected_returns.append(compute_expected_return_for_state_action(state, action, p))
+    policy[state] = np.argmax(expected_returns)
     return max(expected_returns)
 
-def compute_optimal_policy(value_function, gamma, p):
-    """returns a vector of the optimal policy, for a given state the vector contains the optimal value (value to bet)"""
-    policy = np.zeros(100)
-    for state in range(len(policy)):
-        expected_returns = []
-        actions = np.arange(0, min(state, 100 - state) + 1)
-        for action in actions:
-            expected_returns.append(compute_expected_return_for_state_action(state, action, gamma, p, value_function))
-        optimal_action = np.argmax(expected_returns)
-        policy[state] = optimal_action
-    return policy
-        
-def compute_expected_return_for_state_action(state, action, gamma, p, value_function):
-    """computes the expected return for a state action pair using the Bellman equations"""
-    winning_destination_state = state + 2 * action 
-    losing_destination_state = state - action 
-    r = 1 if winning_destination_state == 100 else r = 0
 
-    return p * (r + gamma * value_function[winning_destination_state]) + (1 - p) * (0 + gamma * value_function[losing_destination_state])
+        
+if __name__=="__main__":
+    policy, value = value_iteration(0.4, 1000)
+    print(policy)
+    plt.plot(policy)
+    plt.show()
 
